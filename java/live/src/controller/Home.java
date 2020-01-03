0bf34eca83df1b3730128f8a9fd7ccda2a10e768
ha <ha9011@naver.com>
2020-01-03 09:16:34 +0900

    livesemistart

--- java/live/src/controller/Home.java
+++ java/live/src/controller/Home.java
@@ -11,7 +11,7 @@ import bean.Forward;
 import service.MemberMM;
 import service.ProductMM;
 
-@WebServlet({"/orderlist","/showcart","/cartupdate","/detail","/insertproduct","/menu","/bestItem","/access","/newItem","/joinfrm","/logout","/proupfrm"})
+@WebServlet({"/orderlist"})
 public class Home extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,42 +27,11 @@ public class Home extends HttpServlet {
 			fw = new Forward();
 			fw.setPath("menu.jsp");
 			fw.setRedireact(false);
-			
-		}else if(cmd.equals("/newItem")) {
-			fw = pm.getItemList("new");
-		}else if(cmd.equals("/bestItem")) {
-			fw = pm.getItemList("best");
-			
-		}else if(cmd.equals("/access")) {
-			fw = mm.access();
-		}else if(cmd.equals("/logout")) {
-			fw = mm.logout();
-		}else if(cmd.equals("/proupfrm")) {
-			fw=new Forward();
-			fw.setPath("./product/proUpFrm.jsp");
-		}else if(cmd.equals("/insertproduct")) {
-			
-			fw = pm.insertProduct();
-		}else if(cmd.equals("/detail")) {
-			String codeNum = request.getParameter("name");
-			System.out.println("codeNum : " + codeNum);
-			fw = pm.showDetailInfo(codeNum);
-		}else if(cmd.equals("/cartupdate")) {
-			
-			String cartcode = request.getParameter("cartcode");
-			String cartcount = request.getParameter("count");
-			System.out.println("카트 업데이트");
-			System.out.println("코드 " + cartcode + "갯수 : " + cartcount);
-			
-			fw = pm.insertCart(cartcode, cartcount);
-		
-		}else if(cmd.equals("/showcart")) {
-			fw = pm.showCart();
-		}else if(cmd.equals("/orderlist")) {
-			fw = pm.showOrder();
 		}
 
 
+		
+		
 		
 		if(fw!=null) {
 			if(fw.isRedireact()) {
--- java/live/src/controller/RestController.java
+++ java/live/src/controller/RestController.java
@@ -33,48 +33,6 @@ public class RestController extends HttpServlet {
 			
 			json = pm.getAjaxDetail(pCode);
 			
-		}else if(cmd.equals("/cartdelete")) {
-			String cartDelList = request.getParameter("data");
-			Gson gs=new Gson();
-			ArrayList<ArrayList<String>> dlist = new ArrayList<ArrayList<String>>();
-			dlist=gs.fromJson(cartDelList, new TypeToken<ArrayList<ArrayList<String>>>(){}.getType());
-			
-			json = pm.getAjaxDelete(dlist);
-		}else if(cmd.equals("/orderaddlistcartdel")) {
-			System.out.println("주문확인버튼 클릭후 서블릿 체크");
-			String cartDelList = request.getParameter("data");
-			Gson gs=new Gson();
-			ArrayList<ArrayList<String>> dlist = new ArrayList<ArrayList<String>>();
-			dlist=gs.fromJson(cartDelList, new TypeToken<ArrayList<ArrayList<String>>>(){}.getType());
-			
-			 
-			System.out.println("내용확인 포문");
-			//[new45, 7]  이런식으로 나옴.
-			for(ArrayList<String> a : dlist) {
-				System.out.println(a);
-			}
-			
-			json = pm.getAjaxOrderAddCartDel(dlist);
-			
-		}else if(cmd.equals("/ajaxOrderDetail")) {
-			System.out.println("orderlist Detail 서블릿 체크");
-			String orderDetailList = request.getParameter("oNum");
-			json = pm.getAjaxOrderDetail(orderDetailList);
-		}else if(cmd.equals("/showMemberInfo")) {
-			System.out.println("showMemberInfo 서블릿 체크");
-			String userName = request.getParameter("data");
-			json = pm.getAjaxShowUserInfo(userName);
-		}else if(cmd.equals("/showProductInfo")) {
-			System.out.println("showProductInfo 서블릿 체크");
-			String productCode = request.getParameter("data");
-			json = pm.getAjaxShowProductInfo(productCode);
-		}else if(cmd.equals("/showUserProInfo")) {
-			System.out.println("showUserProInfo 서블릿 체크");
-			String userId = request.getParameter("id");
-			String productCode = request.getParameter("code");
-			System.out.println("유저 : " +  userId + " 코드  : " +  productCode);
-			json = pm.getAjaxshowUserProInfo(userId, productCode);
-			
 		}
 		
 		
